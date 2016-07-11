e586208dbbe238020b968fae05bd96572ae205eb
guowei <guowzou@gmail.com>
2016-07-11 11:38:05 +0800

    init project

---
+++ SimplePod.cpp
@@ -0,0 +1,14 @@
+//
+//  SimplePod.cpp
+//  PodLibraryTest
+//
+//  Created by guowei on 7/11/16.
+//  Copyright © 2016 guowei. All rights reserved.
+//
+
+#include "SimplePod.hpp"
+
+void simplePodPrint()
+{
+    printf("simplePodPrint\n");
+}
---
+++ SimplePod.hpp
@@ -0,0 +1,16 @@
+//
+//  SimplePod.hpp
+//  PodLibraryTest
+//
+//  Created by guowei on 7/11/16.
+//  Copyright © 2016 guowei. All rights reserved.
+//
+
+#ifndef SimplePod_hpp
+#define SimplePod_hpp
+
+#include <stdio.h>
+
+void simplePodPrint();
+
+#endif /* SimplePod_hpp */
